3e1cda9d6ecbba93b467ce800b4dd7bc24afe325
Guillaume Ayoub <guillaume@courtbouillon.org>
2024-07-24 23:27:42 +0200

    Use double quotes everywhere

--- tests/test_tokenizer.py
+++ tests/test_tokenizer.py
@@ -19,55 +19,55 @@ class TokenizerTestParser:
 
         tokenizer.state = getattr(tokenizer, self._state)
         if self._last_start_tag is not None:
-            tokenizer.current_token = {'type': 'startTag', 'name': self._last_start_tag}
+            tokenizer.current_token = {"type": "startTag", "name": self._last_start_tag}
 
         types = {value: key.lower() for key, value in constants.token_types.items()}
         for token in tokenizer:
-            getattr(self, f'process_{types[token["type"]]}')(token)
+            getattr(self, f"process_{types[token["type"]]}")(token)
 
         return self.output_tokens
 
     def process_doctype(self, token):
         self.output_tokens.append([
-            'DOCTYPE', token['name'], token['publicId'],
-            token['systemId'], token['correct']])
+            "DOCTYPE", token["name"], token["publicId"],
+            token["systemId"], token["correct"]])
 
     def process_starttag(self, token):
         self.output_tokens.append([
-            'StartTag', token['name'], token['data'], token['selfClosing']])
+            "StartTag", token["name"], token["data"], token["selfClosing"]])
 
     def process_emptytag(self, token):
-        if token['name'] not in constants.voidElements:
-            self.output_tokens.append('ParseError')
+        if token["name"] not in constants.voidElements:
+            self.output_tokens.append("ParseError")
         self.output_tokens.append([
-            'StartTag', token['name'], dict(token['data'][::-1])])
+            "StartTag", token["name"], dict(token["data"][::-1])])
 
     def process_endtag(self, token):
-        self.output_tokens.append(['EndTag', token['name'], token['selfClosing']])
+        self.output_tokens.append(["EndTag", token["name"], token["selfClosing"]])
 
     def process_comment(self, token):
-        self.output_tokens.append(['Comment', token['data']])
+        self.output_tokens.append(["Comment", token["data"]])
 
     def process_spacecharacters(self, token):
-        self.output_tokens.append(['Character', token['data']])
+        self.output_tokens.append(["Character", token["data"]])
         self.process_spacecharacters = self.process_characters
 
     def process_characters(self, token):
-        self.output_tokens.append(['Character', token['data']])
+        self.output_tokens.append(["Character", token["data"]])
 
     def process_eof(self, token):
         pass
 
     def process_parseerror(self, token):
-        self.output_tokens.append(['ParseError', token['data']])
+        self.output_tokens.append(["ParseError", token["data"]])
 
 
 def concatenate_character_tokens(tokens):
     output_tokens = []
     for token in tokens:
-        if 'ParseError' not in token and token[0] == 'Character':
-            if (output_tokens and 'ParseError' not in output_tokens[-1] and
-                    output_tokens[-1][0] == 'Character'):
+        if "ParseError" not in token and token[0] == "Character":
+            if (output_tokens and "ParseError" not in output_tokens[-1] and
+                    output_tokens[-1][0] == "Character"):
                 output_tokens[-1][1] += token[1]
             else:
                 output_tokens.append(token)
@@ -79,30 +79,30 @@ def concatenate_character_tokens(tokens):
 def tokens_match(expected, received):
     """Test whether the test has passed or failed
 
-    If the ignoreErrorOrder flag is set to true we don't test the relative
+    If the ignoreErrorOrder flag is set to true we don"t test the relative
     positions of parse errors and non parse errors
 
     """
     check_self_closing = False
     for token in expected:
-        if (token[0] == 'StartTag' and len(token) == 4 or
-                token[0] == 'EndTag' and len(token) == 3):
+        if (token[0] == "StartTag" and len(token) == 4 or
+                token[0] == "EndTag" and len(token) == 3):
             check_self_closing = True
             break
 
     if not check_self_closing:
         for token in received:
-            if token[0] == 'StartTag' or token[0] == 'EndTag':
+            if token[0] == "StartTag" or token[0] == "EndTag":
                 token.pop()
 
     # Sort the tokens into two groups: non-parse errors and parse errors.
-    tokens = {'expected': [[], []], 'received': [[], []]}
+    tokens = {"expected": [[], []], "received": [[], []]}
     for token_type, token_list in zip(tokens, (expected, received)):
         for token in token_list:
-            if token != 'ParseError':
+            if token != "ParseError":
                 tokens[token_type][0].append(token)
         tokens[token_type][0] = concatenate_character_tokens(tokens[token_type][0])
-    return tokens['expected'] == tokens['received']
+    return tokens["expected"] == tokens["received"]
 
 
 def decode(inp):
@@ -125,13 +125,13 @@ def decode(inp):
                 return chr(high) + chr(low)
         else:
             return chr(int(m.group(1), 16))
-    return re.compile(r'\\u([0-9A-Fa-f]{4})(?:\\u([0-9A-Fa-f]{4}))?').sub(repl, inp)
+    return re.compile(r"\\u([0-9A-Fa-f]{4})(?:\\u([0-9A-Fa-f]{4}))?").sub(repl, inp)
 
 
 def unescape(test):
-    test['input'] = decode(test['input'])
-    for token in test['output']:
-        if token == 'ParseError':
+    test["input"] = decode(test["input"])
+    for token in test["output"]:
+        if token == "ParseError":
             continue
         else:
             token[1] = decode(token[1])
@@ -143,36 +143,36 @@ def unescape(test):
 
 
 _tests = tuple(
-    (f'{path.stem}-{i}', test)
-    for path in (Path(__file__).parent / 'tokenizer').glob('*.test')
-    for i, test in enumerate(json.loads(path.read_text())['tests']))
+    (f"{path.stem}-{i}", test)
+    for path in (Path(__file__).parent / "tokenizer").glob("*.test")
+    for i, test in enumerate(json.loads(path.read_text())["tests"]))
 _xfails = (
-    'domjs-4',
-    'test2-0',
-    'test3-280', 'test3-283', 'test3-284', 'test3-286', 'test3-287', 'test3-289',
-    'test3-292', 'test3-293', 'test3-295', 'test3-296', 'test3-298', 'test3-310',
-    'test3-718',
+    "domjs-4",
+    "test2-0",
+    "test3-280", "test3-283", "test3-284", "test3-286", "test3-287", "test3-289",
+    "test3-292", "test3-293", "test3-295", "test3-296", "test3-298", "test3-310",
+    "test3-718",
 )
 
 
-@pytest.mark.parametrize('id, test', _tests, ids=(id for id, _ in _tests))
+@pytest.mark.parametrize("id, test", _tests, ids=(id for id, _ in _tests))
 def test_tokenizer(id, test):
     if id in _xfails:
         pytest.xfail()
-    if 'initialStates' not in test:
-        test['initialStates'] = ['Data state']
-    if 'doubleEscaped' in test:
+    if "initialStates" not in test:
+        test["initialStates"] = ["Data state"]
+    if "doubleEscaped" in test:
         test = unescape(test)
-    for initial_state in test.get('initialStates'):
-        initial_state = re.compile(r'\W+(\w)').sub(
-            lambda match: f'_{match.group(1)}', initial_state.lower())
-        expected = test['output']
-        parser = TokenizerTestParser(initial_state, test.get('lastStartTag'))
-        tokens = parser.parse(test['input'])
-        received = [token[0] if token[0] == 'ParseError' else token for token in tokens]
+    for initial_state in test.get("initialStates"):
+        initial_state = re.compile(r"\W+(\w)").sub(
+            lambda match: f"_{match.group(1)}", initial_state.lower())
+        expected = test["output"]
+        parser = TokenizerTestParser(initial_state, test.get("lastStartTag"))
+        tokens = parser.parse(test["input"])
+        received = [token[0] if token[0] == "ParseError" else token for token in tokens]
         error_message = (
-            f'\nInitial state: {initial_state}',
-            f'\nInput: {test['input']}',
-            f'\nExpected: {expected!r}',
-            f'\nReceived: {tokens!r}')
+            f"\nInitial state: {initial_state}",
+            f"\nInput: {test["input"]}",
+            f"\nExpected: {expected!r}",
+            f"\nReceived: {tokens!r}")
         assert tokens_match(expected, received), error_message
--- tests/test_tree_construction.py
+++ tests/test_tree_construction.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from warnings import warn
 from xml.etree import ElementTree
 
 import pytest
@@ -9,26 +10,26 @@ from . import Data
 
 
 def convert(data, strip_chars):
-    return (line[strip_chars:] if line.startswith('|') else line for line in data)
+    return (line[strip_chars:] if line.startswith("|") else line for line in data)
 
 
 def serialize(item, indent=0, skip=False):
     if item.tag == ElementTree.Comment:
-        yield f'{"  " * indent}<!-- {item.text} -->'
+        yield f"{'  ' * indent}<!-- {item.text} -->"
         return
-    elif item.tag == '<!DOCTYPE>':
+    elif item.tag == "<!DOCTYPE>":
         text = item.text
-        if public := item.get('publicId'):
+        if public := item.get("publicId"):
             public = f"'{public}'" if '"' in public else f'"{public}"'
-            text += f' PUBLIC {public}' if item.text == 'html' else f' {public} ""'
-        elif system := item.get('systemId'):
+            text += f" PUBLIC {public}" if item.text == "html" else f' {public} ""'
+        elif system := item.get("systemId"):
             system = f'"{system}"'
-            text += ' SYSTEM' if item.text == 'html' else ' ""'
-            text += f' {system}'
-        yield f'<!DOCTYPE {text}>'
+            text += " SYSTEM" if item.text == "html" else ' ""'
+            text += f" {system}"
+        yield f"<!DOCTYPE {text}>"
         return
     if not skip:
-        yield f'{"  " * indent}<{item.tag}>'
+        yield f"{'  ' * indent}<{item.tag}>"
         indent += 1
         for key, value in sorted(item.attrib.items()):
             yield f'{"  " * indent}{key}="{value}"'
@@ -41,98 +42,101 @@ def serialize(item, indent=0, skip=False):
 
 
 _tests = tuple(
-    (f'{path.stem}-{i}', test)
-    for path in (Path(__file__).parent / 'tree-construction').glob('*.dat')
-    for i, test in enumerate(Data(path)) if 'script-on' not in test)
+    (f"{path.stem}-{i}", test)
+    for path in (Path(__file__).parent / "tree-construction").glob("*.dat")
+    for i, test in enumerate(Data(path)) if "script-on" not in test)
 _xfails = (
-    'adoption01-17',
+    "adoption01-17",
 
-    'blocks-12',
+    "blocks-12",
 
-    'foreign-fragment-2', 'foreign-fragment-3', 'foreign-fragment-18',
-    'foreign-fragment-19', 'foreign-fragment-22', 'foreign-fragment-23',
-    'foreign-fragment-26', 'foreign-fragment-27', 'foreign-fragment-30',
-    'foreign-fragment-31', 'foreign-fragment-34', 'foreign-fragment-35',
-    'foreign-fragment-39', 'foreign-fragment-41', 'foreign-fragment-60',
-    'foreign-fragment-61', 'foreign-fragment-62', 'foreign-fragment-63',
-    'foreign-fragment-64', 'foreign-fragment-66',
+    "foreign-fragment-2", "foreign-fragment-3", "foreign-fragment-18",
+    "foreign-fragment-19", "foreign-fragment-22", "foreign-fragment-23",
+    "foreign-fragment-26", "foreign-fragment-27", "foreign-fragment-30",
+    "foreign-fragment-31", "foreign-fragment-34", "foreign-fragment-35",
+    "foreign-fragment-39", "foreign-fragment-41", "foreign-fragment-60",
+    "foreign-fragment-61", "foreign-fragment-62", "foreign-fragment-63",
+    "foreign-fragment-66",
 
-    'isindex-0', 'isindex-1', 'isindex-2', 'isindex-3',
+    "isindex-0", "isindex-1", "isindex-2", "isindex-3",
 
-    'namespace-sensitivity-0',
+    "namespace-sensitivity-0",
 
-    'ruby-0', 'ruby-1', 'ruby-2', 'ruby-3', 'ruby-5', 'ruby-7',
-    'ruby-10', 'ruby-12', 'ruby-15', 'ruby-17', 'ruby-20',
+    "ruby-0", "ruby-1", "ruby-2", "ruby-3", "ruby-5", "ruby-7",
+    "ruby-10", "ruby-12", "ruby-15", "ruby-17", "ruby-20",
 
-    *[f'template-{i}' for i in range(7)],
-    *[f'template-{i}' for i in range(8, 39)],
-    *[f'template-{i}' for i in range(40, 111)],
+    *[f"template-{i}" for i in range(7)],
+    *[f"template-{i}" for i in range(8, 39)],
+    *[f"template-{i}" for i in range(40, 111)],
 
-    'tests2-6', 'tests2-7',
+    "tests2-6", "tests2-7",
 
-    'tests8-5',
+    "tests8-5",
 
-    'tests11-2', 'tests11-4', 'tests11-5', 'tests11-6',
+    "tests11-2", "tests11-4", "tests11-5", "tests11-6",
 
-    'tests18-15',
+    "tests18-15",
 
-    'tests19-7', 'tests19-14', 'tests19-17',
+    "tests19-7", "tests19-14", "tests19-17",
 
-    'tests25-7',
+    "tests25-7",
 
-    'tests26-16', 'tests26-17', 'tests26-18', 'tests26-19',
+    "tests26-16", "tests26-17", "tests26-18", "tests26-19",
 
-    'webik02-14', 'webik02-15', 'webik02-16',
+    "webik02-14", "webik02-15", "webik02-16",
 
     # Introduced by the fork.
 
     # Doctype.
-    'doctype01-26', 'doctype01-28', 'doctype01-29', 'doctype01-31', 'doctype01-32',
-    'doctype01-33', 'doctype01-34', 'doctype01-35', 'doctype01-36',
-    'tests6-46',
-    'quirks01-0', 'quirks01-1', 'quirks01-2', 'quirks01-3',
+    "doctype01-26", "doctype01-28", "doctype01-29", "doctype01-31", "doctype01-32",
+    "doctype01-33", "doctype01-34", "doctype01-35", "doctype01-36",
+    "tests6-46",
+    "quirks01-0", "quirks01-1", "quirks01-2", "quirks01-3",
 
     # <template>, <search>.
-    'template-111', 'search-element-0', 'search-element-1',
+    "template-111", "search-element-0", "search-element-1",
 
     # Mismatched elements.
-    'webkit02-11', 'webkit02-15', 'webkit02-16', 'webkit02-17',
+    "webkit02-11", "webkit02-15", "webkit02-16", "webkit02-17",
 
     # Attributes namespace.
-    'webkit02-22',
+    "webkit02-22",
 
     # <hr> in <options>.
-    *[f'webkit02-{i}' for i in range(25, 35)],
+    *[f"webkit02-{i}" for i in range(25, 35)],
 
     # Namespace in fragment.
-    'foreign-fragment-59', 'foreign-fragment-65',
+    "foreign-fragment-59", "foreign-fragment-65",
 )
 
-@pytest.mark.parametrize('namespace', (False, True), ids=('nons', 'ns'))
-@pytest.mark.parametrize('id, test', _tests, ids=(id for id, _ in _tests))
+@pytest.mark.parametrize("namespace", (False, True), ids=("nons", "ns"))
+@pytest.mark.parametrize("id, test", _tests, ids=(id for id, _ in _tests))
 def test_tree_construction(id, test, namespace):
-    if id in _xfails:
-        pytest.xfail()
-
     # TODO: Check error messages.
     parser = HTMLParser(namespace_html_elements=namespace)
 
-    input = test['data']
-    container = test['document-fragment']
-    expected = '\n'.join(convert(test['document'].split('\n'), 2))
+    input = test["data"]
+    container = test["document-fragment"]
+    expected = "\n".join(convert(test["document"].split("\n"), 2))
 
     if container:
         document = parser.parse_fragment(input, container=container)
     else:
         document = parser.parse(input, full_tree=True)
 
-    output = '\n'.join(convert(serialize(document, skip=True), 3))
+    output = "\n".join(convert(serialize(document, skip=True), 3))
     for prefix, value in constants.prefixes.items():
-        output = output.replace(f'{{{prefix}}}', '' if value == 'html' else f'{value} ')
+        output = output.replace(f"{{{prefix}}}", "" if value == "html" else f"{value} ")
 
-    error_message = ''.join(
-        f'\n{line}\n' for line in (
-            f'Input:\n{input}',
-            f'Expected:\n{expected}',
-            f'Received:\n{output}'))
-    assert expected == output, error_message
+    if id in _xfails:
+        if expected == output:
+            warn(f"{id}{"-ns" if namespace else ""} passes but is marked as xfail")
+        else:
+            pytest.xfail()
+    else:
+        error_message = "".join(
+            f"\n{line}\n" for line in (
+                f"Input:\n{input}",
+                f"Expected:\n{expected}",
+                f"Received:\n{output}"))
+        assert expected == output, error_message
